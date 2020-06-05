995148eaff10895966184bfd1b1bbb84396379d9
Stéphane PATERNA <paterna@club-internet.fr>
2020-06-05 19:42:36 +0200

    Réalisation front-end page home/connexion

--- database/database.js
+++ database/database.js
@@ -6,10 +6,10 @@ function connectToDB() {
   if (connection) return connection;
 
  connection = mysql.createConnection({
-    host     : "localhost",
-    user     : "root",
-    password : "root",
-    database : "twitterlike", 
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
   });
 
   connection.connect(function (err) {
